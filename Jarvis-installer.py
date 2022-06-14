8d040a5fd25f796065bd72da36c6ceb5d54864c8
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2022-06-14 09:37:51 -0300

    Add files via upload

---
+++ Jarvis-installer.py
@@ -0,0 +1,63 @@
+import time
+
+def um():
+    os.system("pip install selenium")
+    # print("pip install selenium")
+    time.sleep(40)
+def dois():
+    os.system("pip install time")
+    # print("pip install time")
+    time.sleep(40)
+def tres():
+    os.system("pip install termcolor")
+    # print("pip install termcolor")
+    time.sleep(40)
+def quatro():
+    os.system("pip install tkinter")
+    # print("pip install tkinter")
+    time.sleep(40)
+def cinco():
+    os.system("pip install Pillow")
+    # print("pip install Pillow")
+    time.sleep(40)
+
+def seis():
+    os.system("pip install customtkinter")
+    # print("pip install customtkinter")
+    time.sleep(40)
+
+def sete():
+    os.system("pip install requests")
+    # print("pip install requests")
+    time.sleep(40)
+
+def oito():
+    os.system("pip install numpy")
+    # print("pip install numpy")
+    time.sleep(40)
+
+# x = threading.Thread(target=um)
+# x.start()
+#
+# y = threading.Thread(target=dois)
+# y.start()
+#
+# z = threading.Thread(target=tres)
+# z.start()
+#
+# w = threading.Thread(target=quatro)
+# w.start()
+#
+# v = threading.Thread(target=cinco)
+# v.start()
+#
+um()
+dois()
+tres()
+quatro()
+cinco()
+seis()
+sete()
+oito()
+
+
