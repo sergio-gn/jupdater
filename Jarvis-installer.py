ca76c81a102fff3844112df8b5fb58efca7b44c3
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2022-06-14 10:29:11 -0300

    Add files via upload

--- Jarvis-installer.py
+++ Jarvis-installer.py
@@ -1,40 +1,41 @@
 import time
+import os
 
 def um():
     os.system("pip install selenium")
     # print("pip install selenium")
-    time.sleep(40)
+    time.sleep(20)
 def dois():
     os.system("pip install time")
     # print("pip install time")
-    time.sleep(40)
+    time.sleep(20)
 def tres():
     os.system("pip install termcolor")
     # print("pip install termcolor")
-    time.sleep(40)
+    time.sleep(20)
 def quatro():
     os.system("pip install tkinter")
     # print("pip install tkinter")
-    time.sleep(40)
+    time.sleep(20)
 def cinco():
     os.system("pip install Pillow")
     # print("pip install Pillow")
-    time.sleep(40)
+    time.sleep(20)
 
 def seis():
     os.system("pip install customtkinter")
     # print("pip install customtkinter")
-    time.sleep(40)
+    time.sleep(20)
 
 def sete():
     os.system("pip install requests")
     # print("pip install requests")
-    time.sleep(40)
+    time.sleep(20)
 
 def oito():
     os.system("pip install numpy")
     # print("pip install numpy")
-    time.sleep(40)
+    time.sleep(20)
 
 # x = threading.Thread(target=um)
 # x.start()
